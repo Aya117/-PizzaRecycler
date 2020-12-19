5156675a0b45cb8afb30b7f12064a445d983d1ff
Alameer <ayataharimawi@gmail.com>
2020-12-19 21:01:52 +0200

    virsion 1.0

---
+++ app/src/main/java/edu/cs/birzeit/pizzarecicler/MainActivity.java
@@ -0,0 +1,30 @@
+package edu.cs.birzeit.pizzarecicler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //test comment
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recycler = (RecyclerView)findViewById(R.id.pizza_recycler);
+
+        String[] captions = new String[Pizza.pizzas.length];
+        int[] ids = new int[Pizza.pizzas.length];
+
+        for(int i = 0; i<captions.length;i++){
+            captions[i] = Pizza.pizzas[i].getName();
+            ids[i] = Pizza.pizzas[i].getImageID();
+        }
+        recycler.setLayoutManager(new LinearLayoutManager(this));
+        CaptionedImagesAdapter adapter = new CaptionedImagesAdapter(captions, ids);
+        recycler.setAdapter(adapter);
+    }
+}
